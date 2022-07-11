725e69c0cd4eb05249f2006e2390202d1c9dfec1
victor toupar <victor.toupar@gmail.com>
2022-07-11 19:24:03 +0800

    Added background in homepage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -6,8 +6,15 @@ export default function Home(props) {
     <div className="home">
         <Header />
         <div className="container">
+          <div className="poste">
+              <p>Poste recherché</p>
+              <hr></hr>
+              <p>Designeur UI/UX Junior</p>
+          </div>
           <p>Après avoir designé des interfaces à Taïwan pendant deux ans pour des applications web Big Data, je souhaite poursuivre ma carrière dans ce domaine.</p>
+          <div className="bg-square"></div>
         </div>
+        
         
     </div>
   )
