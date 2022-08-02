36ab9ceeada586472a4829b926e1a6db716c8f24
victor toupar <victor.toupar@gmail.com>
2022-08-02 19:19:29 +0800

    add 3 buttons to download resume

--- src/routes/Cv.jsx
+++ src/routes/Cv.jsx
@@ -1,6 +1,10 @@
 import "../styles/cv.less"
 import Header from "../components/Header"
 
+
+
+
+
 export default function Cv(props) {
   return (
     <div className="cv">
@@ -10,16 +14,22 @@ export default function Cv(props) {
             <div className="avatar"></div>
             <div className="infos-container">
               <h1>Bonjour !👋🏼</h1>
-              <h3>Moi, c'est <strong>Victor Toupar.</strong></h3>
+              <h3>Moi, c'est <b>Victor Toupar.</b></h3>
               <p>Je suis un Designeur UI/UX autodidacte depuis 2 ans. J’habite actuellement à Taïwan.<br />
 J’aime voyager 🛫, ️le jeux vidéo ‍🎮, la moto 🏍, et la compétition 🥇. </p>
             </div>
           </div>
           <p>Tu souhaites en apprendre d’avantage sur mes compétences ? <strong>Télécharges mon CV !</strong></p>
           <div className="resume-container">
-            <div className="resume"></div>
-            <div className="resume"></div>
-            <div className="resume"></div>
+            <div className="resume">
+              <button className="download-btn">Télécharger en Français 🇫🇷</button>
+            </div>
+            <div className="resume">
+              <button className="download-btn">Télécharger en Anglais 🇬🇧</button>
+            </div>
+            <div className="resume">
+              <button className="download-btn">Télécharger en Chinois 🇹🇼</button>
+            </div>
           </div>
 
       </div>
