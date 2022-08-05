a2ea1390e4018f7ce423c347e145b0e30843f850
victor toupar <victor.toupar@gmail.com>
2022-08-05 18:16:05 +0800

    add new component ProjectCard

---
+++ src/components/ProjectCard.jsx
@@ -0,0 +1,10 @@
+import "../styles/header.less";
+
+const ProjectCard = () => {
+    return (
+        <h1>ProjectCard</h1>
+    );
+
+}
+
+export default ProjectCard;
--- src/routes/Projets.jsx
+++ src/routes/Projets.jsx
@@ -1,13 +1,30 @@
 import "../styles/projets.less"
 import Header from "../components/Header";
+import ProjectCard from "../components/ProjectCard";
 
 export default function Projets(props) {
   return (
     <div className="projets">
       <Header />
-      <h1>MES PROJETS</h1>
-      <h1>MES PROJETS</h1>
-      <h1>MES PROJETS</h1>
+      {//need to do a .map of all the <ProjectCard> and show the card with the right data
+      }
+
+
+
+
+
+      {//for testing design
+      }
+      <div className="container">
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+        <ProjectCard />
+      </div>
+      
+
 
     </div>
   )
